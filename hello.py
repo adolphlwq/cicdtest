077ab36a2f7cadee721dbc78f9d2d0cadfd6f8d3
adolphlwq <kenan3015@gmail.com>
2016-12-02 16:48:59 +0800

    add health endpoint

--- hello.py
+++ hello.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return "hello world"
 
+@app.route('/health')
+def health():
+    return "health"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
